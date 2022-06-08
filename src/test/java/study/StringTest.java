5048e2b066e0e2b4555a726417336e3ef565d328
taegyung <stk346@gmail.com>
2022-06-08 23:09:08 +0900

    feat: : 문자열 계산기 - split함수 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,9 +5,24 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+    // 딴위 테스트란 클래스의 퍼블릭메소드를 대상으로 그 함수가 제대로 만들어졌는지 예제상황을 추가하는것.
     @Test
     void replace() {
+        String actual = "abc".replace("b", "/");
+        assertThat(actual).isEqualTo("a/c");
+    }
+
+    @Test
+    void replace_XMFFUtMFeO() {
         String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+        assertThat(actual).isEqualTo("aAdc");
+
+    }
+
+    @Test
+    void split_test() {
+        String[] actual = "1,2".split(",");
+        String[] expected = new String[] {"1","2"};
+        assertThat(actual).isEqualTo(expected);
     }
 }
